e7c240365cd99638df080d868ef5615e9f66932c
Dolf Andringa <dolfandringa@gmail.com>
2019-04-30 17:43:57 +0800

    Routes and menu are now only defined in dashboard.routes.ts. Sidebar uses the routes to setup the menuitems. route-guard.ts uses loginService.hasUrlAccess to determine if user has access sidebar uses the same way to determine which menuitems to show.

--- src/app/route-guard.ts
+++ src/app/route-guard.ts
@@ -1,37 +1,53 @@
 import { Injectable }     from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router }    from '@angular/router';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, Router }    from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment'
 
 import { LoginService } from './login/login.service';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable()
 export class AuthGuard implements CanActivate 
 {
 
-	constructor( private loginSevice: LoginService, private router: Router, private cookieService: CookieService, private http: HttpClient ){}
+	constructor( private loginService: LoginService, private router: Router, private cookieService: CookieService, private http: HttpClient ){}
   private baseUrl = environment.backend_uri;
 
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>
   {
-    let url: string = state.url;
-
-    return this.checkLogin(url);   
+    if('data' in route && route.data.accessUrl != undefined){
+      let redirUrl = state.url;
+      let accessUrl: string = route.data.accessUrl
+      return this.checkLogin(accessUrl, redirUrl);   
+    } 
+    else {
+      return of(true);
+    }
   }
 
-  checkLogin(url: string): boolean 
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>
+  {
+    return this.canActivate(route, state);
+  }
+
+  checkLogin(accessUrl: string, redirUrl: string): Observable<boolean> 
   {
     if (this.cookieService.check("token"))
     {
-      return true
+      return this.loginService.hasUrlAccess(accessUrl).pipe(map( response => {
+        console.log('access to',accessUrl,':',response);
+        return response;
+      }));
     }
     else
     {
-      this.loginSevice.redirectUrl = url
+      this.loginService.redirectUrl = redirUrl;
       this.router.navigate(['/login']);
-      return false;
+      return of(false);
     }
   }
 
